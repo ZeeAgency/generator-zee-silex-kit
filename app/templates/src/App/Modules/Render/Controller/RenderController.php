8e12bc96710f87d6ae818baacf16a4b6aeb651ac
Romain Jacquart <rjacquart@zeeagency.com>
2015-05-18 10:51:23 +0200

    Update RenderController example to extends Zee\Controller

--- app/templates/src/App/Modules/Render/Controller/RenderController.php
+++ app/templates/src/App/Modules/Render/Controller/RenderController.php
@@ -1,22 +1,12 @@
 <?php
 namespace App\Modules\Render\Controller;
 
-use Silex\Application;
 use App\Modules\Render\Module;
+use Silex\Application;
+use Zee;
 
-class RenderController
+class RenderController extends Zee\Controller
 {
-    private $module;
-
-    /** @var \Twig_Environment $twig */
-    private $twig;
-
-    public function __construct(Application $app, Module $module)
-    {
-        $this->module = $module;
-        $this->twig = $module['twig'];
-    }
-
     public function indexAction(Application $app)
     {
         return $app['twig']->render('pages/index.twig');
